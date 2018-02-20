4584a717f0978ccae314b7a87b133bcd8b5a4839
Josh NG <joshng@hotmail.co.uk>
2018-02-20 04:36:39 +0000

    Added a small tutorial i found online to upload files

---
+++ AgileTeam6/AgileTeam6Website/NewProject.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="NewProject.aspx.cs" Inherits="NewProject" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    
+    <form id="Form1" method="post" enctype="multipart/form-data" runat="server">
+        <h1>NewProject</h1>
+        ProjectName:
+        <asp:TextBox id="ProjectName" runat="server" type="text" />
+        <input type="file" id="File1" name="File1" runat="server" />
+        <br />
+        <input type="submit" id="Submit1" value="Upload" runat="server" />
+</form>
+</body>
+</html>
---
+++ AgileTeam6/AgileTeam6Website/NewProject.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class NewProject : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Submit1_ServerClick(object sender, System.EventArgs e)
+    {
+        if ((File1.PostedFile != null) && (File1.PostedFile.ContentLength > 0))
+        {
+            string fn = System.IO.Path.GetFileName(File1.PostedFile.FileName);
+            string SaveLocation = Server.MapPath("Projects") + "\\" + fn;
+            try
+            {
+                File1.PostedFile.SaveAs(SaveLocation);
+                Response.Write("The file has been uploaded.");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: " + ex.Message);
+                //Note: Exception.Message returns a detailed message that describes the current exception. 
+                //For security reasons, we do not recommend that you return Exception.Message to end users in 
+                //production environments. It would be better to put a generic error message. 
+            }
+        }
+        else
+        {
+            Response.Write("Please select a file to upload.");
+        }
+    }
+}
